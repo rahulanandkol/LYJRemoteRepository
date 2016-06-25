c0123a3bb146709e2c1ee6fa72744c7fd9408c6e
lenovo <lenovo@lenovo-PC>
2016-06-25 18:03:55 +0530

    committing my first change

--- LOVEYOUJAVA/src/com/test/LYJ/TestingRepository.java
+++ LOVEYOUJAVA/src/com/test/LYJ/TestingRepository.java
@@ -4,6 +4,7 @@ public class TestingRepository {
 	public static void main (String args[])
 	{
 		System.out.println("My first commit to LYJ Repository");
+		System.out.println("committing a change");
 	}
 
 }
