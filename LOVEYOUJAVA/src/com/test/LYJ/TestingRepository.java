34778d35421c9ce4efd5e4ab7e16c301c2d1bae2
palljiten <palljiten@gmail.com>
2016-08-06 15:17:26 +0530

    committing from jitu's end

--- LOVEYOUJAVA/src/com/test/LYJ/TestingRepository.java
+++ LOVEYOUJAVA/src/com/test/LYJ/TestingRepository.java
@@ -3,7 +3,7 @@ package com.test.LYJ;
 public class TestingRepository {
 	public static void main (String args[])
 	{
-		System.out.println("My first commit to LYJ Repository");
+		System.out.println("jitu commits to LYJ Repository");
 		System.out.println("committing a change");
 	}
 
